a53d63be1afef15140f844cc93f7c53ae33bf9cc
LaithH10 <laiethhadid172@gmail.com>
2025-02-24 20:18:57 +0100

    Property page

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
-
 export const api = axios.create({
   baseURL: "http://localhost:8000/api",
 });
@@ -10,6 +9,22 @@ export const api = axios.create({
 export const getAllProperties = async () => {
   try {
     const response = await api.get("/residency/allresd", {
+      timeout: 10 * 1000,
+    });
+
+    if (response.status === 400 || response.status === 500) {
+      throw response.data;
+    }
+    return response.data;
+  } catch (error) {
+    toast.error("Something went wrong");
+    throw error;
+  }
+};
+
+export const getProperty = async (id) => {
+  try {
+    const response = await api.get(`/residency/${id}`, {
       timeout: 10 * 1000,
     });
 
